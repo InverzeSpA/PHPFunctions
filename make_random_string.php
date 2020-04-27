ae866a91f29f4e8faed889f5ffba07cd68833af4
J <51800369+InverzeSpA@users.noreply.github.com>
2020-04-26 21:53:03 -0400

    Create make_random_string.php

---
+++ make_random_string.php
@@ -0,0 +1,17 @@
+//make random string
+if (!function_exists("make_random_string")) {
+
+    function make_random_string($length = 10) {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $characters_length = strlen($characters);
+        $random_string = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $random_string .= $characters[rand(0, $characters_length - 1)];
+        }
+
+        return $random_string;
+
+    }
+
+}
