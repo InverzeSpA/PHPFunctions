16e0e12df3b7856ba5f7351f11a4c004f74c1e40
J <51800369+InverzeSpA@users.noreply.github.com>
2020-04-26 21:47:12 -0400

    Create get_array_value.php

---
+++ get_array_value.php
@@ -0,0 +1,15 @@
+/**
+ * check the array key and return the value
+ *
+ * @param array $array
+ * @return extract array value safely
+ */
+if (!function_exists('get_array_value')) {
+
+    function get_array_value(array $array, $key) {
+        if (array_key_exists($key, $array)) {
+            return $array[$key];
+        }
+    }
+
+}
