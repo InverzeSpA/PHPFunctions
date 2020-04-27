57d3cde7fb8b6843964557ed0c1f139189665bce
J <51800369+InverzeSpA@users.noreply.github.com>
2020-04-26 22:06:29 -0400

    Create convert_time_string_to_second.php

---
+++ convert_time_string_to_second.php
@@ -0,0 +1,14 @@
+/**
+ * get seconds form a given time string
+ * 
+ * @param string $time
+ * @return seconds
+ */
+if (!function_exists('convert_time_string_to_second')) {
+
+    function convert_time_string_to_second($time = "00:00:00") {
+        $hms = explode(":", $time);
+        return $hms[0] * 3600 + ($hms[1] * 60) + ($hms[2]);
+    }
+
+}
