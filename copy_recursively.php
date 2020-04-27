1a3cb60adb69441db7b0c56b463a3f0669555349
J <51800369+InverzeSpA@users.noreply.github.com>
2020-04-26 22:14:53 -0400

    Create copy_recursively.php

---
+++ copy_recursively.php
@@ -0,0 +1,25 @@
+/**
+ * copy a directory to another directory
+ * 
+ * @param string $src
+ * @param string $dst
+ * @return void
+ */
+if (!function_exists('copy_recursively')) {
+
+    function copy_recursively($src, $dst) {
+        $dir = opendir($src);
+        @mkdir($dst);
+        while (false !== ( $file = readdir($dir))) {
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if (is_dir($src . '/' . $file)) {
+                    copy_recursively($src . '/' . $file, $dst . '/' . $file);
+                } else {
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+
+}
