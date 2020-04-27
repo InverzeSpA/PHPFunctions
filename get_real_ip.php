a1beaa63b415db4000fe9ea602abd3e8f867c861
J <51800369+InverzeSpA@users.noreply.github.com>
2020-04-26 21:49:33 -0400

    Create get_real_ip.php

---
+++ get_real_ip.php
@@ -0,0 +1,19 @@
+/**
+ * get users ip address
+ *
+ * @return ip
+ */
+if (!function_exists('get_real_ip')) {
+
+    function get_real_ip() {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+
+}
